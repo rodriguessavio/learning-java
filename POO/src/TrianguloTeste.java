960440e11ccca835e5ca5f7c2cf3dbb321b14465
rodriguessavio <capar2022113TADS0049@aluno.ifpi.edu.br>
2023-08-28 09:37:15 -0300

    Triangle

--- POO/src/TrianguloTeste.java
+++ POO/src/TrianguloTeste.java
@@ -1,12 +1,28 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class TrianguloTeste {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
 
-        Triangulo a = new Triangulo(3, 4, 5);
-        Triangulo b = new Triangulo(7.5, 4.5, 4.2);
-        System.out.println(a.calcularArea());
+        Triangulo a, b;
+        a = new Triangulo();
+        b = new Triangulo();
+
+        System.out.println("Informe os valores do triangulo A: ");
+        a.ladoA = entrada.nextDouble();
+        a.ladoB = entrada.nextDouble();
+        a.ladoC = entrada.nextDouble();
+
+        System.out.println("Informe os valores do triangulo B: ");
+        b.ladoA = entrada.nextDouble();
+        b.ladoB = entrada.nextDouble();
+        b.ladoC = entrada.nextDouble();
+
+        entrada.close();
+
+        System.out.printf("área = %.2f", a.calcularArea());
         System.out.printf("área = %.2f", b.calcularArea());
     }
 }
